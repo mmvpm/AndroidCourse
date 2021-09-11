82ae068412eef86a8739ffdba8f0fcfb53af5978
IdeaSeeker <stroganov.n.36@gmail.com>
2021-09-11 21:07:11 +0300

    add dividers to list of users

--- app/src/main/java/com/nikitastroganov/androidcourse/MainActivity.kt
+++ app/src/main/java/com/nikitastroganov/androidcourse/MainActivity.kt
@@ -4,18 +4,23 @@ import android.annotation.SuppressLint
 import android.net.Uri
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.DividerItemDecoration
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 
 
 class MainActivity : AppCompatActivity(R.layout.activity_main) {
 
-    @SuppressLint("NotifyDataSetChanged")
+    @SuppressLint("NotifyDataSetChanged", "UseCompatLoadingForDrawables")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         val recyclerView: RecyclerView = findViewById(R.id.usersRecyclerView)
         recyclerView.layoutManager = LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false)
+
+        val dividerItemDecoration = DividerItemDecoration(this, LinearLayoutManager.VERTICAL)
+        dividerItemDecoration.setDrawable(getDrawable(R.drawable.item_user_divider)!!)
+        recyclerView.addItemDecoration(dividerItemDecoration)
 
         val adapter = UserAdapter()
         recyclerView.adapter = adapter
