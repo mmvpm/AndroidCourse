9672437aff3615d90f03df4c3808d07cc3694f4f
IdeaSeeker <stroganov.n.36@gmail.com>
2021-11-10 14:47:18 +0300

    add sign up fake email confirmation

--- app/src/main/java/com/nikitastroganov/androidcourse/ui/emailconfirmation/EmailConfirmationFragment.kt
+++ app/src/main/java/com/nikitastroganov/androidcourse/ui/emailconfirmation/EmailConfirmationFragment.kt
@@ -1,14 +1,24 @@
 package com.nikitastroganov.androidcourse.ui.emailconfirmation
 
+import android.os.Bundle
+import android.view.View
 import androidx.fragment.app.viewModels
+import androidx.navigation.fragment.findNavController
 import by.kirich1409.viewbindingdelegate.viewBinding
-import com.nikitastroganov.androidcourse.ui.base.BaseFragment
 import com.nikitastroganov.androidcourse.R
 import com.nikitastroganov.androidcourse.databinding.FragmentEmailConfirmationBinding
+import com.nikitastroganov.androidcourse.ui.base.BaseFragment
 
 class EmailConfirmationFragment : BaseFragment(R.layout.fragment_email_confirmation) {
 
     private val viewBinding by viewBinding(FragmentEmailConfirmationBinding::bind)
 
     private val viewModel: EmailConfirmationViewModel by viewModels()
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        viewBinding.backButton.setOnClickListener {
+            findNavController().popBackStack()
+        }
+    }
 }
