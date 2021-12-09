e4bbf33cb8abbf103459c44f9f0f02b20b6a4f8f
IdeaSeeker <stroganov.n.36@gmail.com>
2021-12-10 00:07:18 +0300

    add mock user profile

--- app/src/main/java/com/nikitastroganov/androidcourse/ui/profiles/ProfileViewModel.kt
+++ app/src/main/java/com/nikitastroganov/androidcourse/ui/profiles/ProfileViewModel.kt
@@ -1,6 +1,7 @@
 package com.nikitastroganov.androidcourse.ui.profiles
 
 import androidx.lifecycle.viewModelScope
+import com.nikitastroganov.androidcourse.entity.User
 import com.nikitastroganov.androidcourse.interactor.AuthInteractor
 import com.nikitastroganov.androidcourse.ui.base.BaseViewModel
 import dagger.hilt.android.lifecycle.HiltViewModel
@@ -31,6 +32,18 @@ class ProfileViewModel @Inject constructor(
                 _eventChannel.send(Event.LogoutError(error))
             }
         }
+    }
+
+    fun getUser(): User {
+        // mock
+        return User(
+            id = 0,
+            userName = "vasyapupkin228",
+            firstName = "Василий",
+            lastName = "Пупкин",
+            avatarUri = "https://www.thiswaifudoesnotexist.net/example-55555.jpg",
+            groupName = null
+        )
     }
 
     sealed class Event {
